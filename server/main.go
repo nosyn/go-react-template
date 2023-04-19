b3701ee1e572cd290629837f1972e3a8b05bd16b
Son Yen Nguyen <nosyn@Sons-MBP.attlocal.net>
2023-04-18 20:32:13 -0500

    Update go server

--- server/main.go
+++ server/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -10,15 +9,7 @@ import (
 func main() {
 	r := gin.Default()
 
-	r.GET("/", func(c *gin.Context) {
-		fmt.Println("roottttt")
-		c.JSON(http.StatusOK, gin.H{
-			"message": "roott",
-		})
-	})
-
 	r.GET("/ping", func(c *gin.Context) {
-		fmt.Println("hellooo")
 		c.JSON(http.StatusOK, gin.H{
 			"message": "pong",
 		})
